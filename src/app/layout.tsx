085235c9d3bbd7db405db6d0519de6f5d3aa9660
dasom <ds92ko@gmail.com>
2025-07-10 23:56:04 +0900

    chore: prettier 설정 추가 및 전체 코드 포맷 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { fredoka, pressStart2p } from './fonts';
 
 export const metadata: Metadata = {
   title: 'Pokédex',
-  description: '모든 세대의 포켓몬 정보를 한눈에! 이름, 타입, 능력치를 검색하고, 나만의 포켓몬을 찾아보세요.'
+  description:
+    '모든 세대의 포켓몬 정보를 한눈에! 이름, 타입, 능력치를 검색하고, 나만의 포켓몬을 찾아보세요.'
 };
 
 export default function RootLayout({
@@ -12,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ko'>
+    <html lang="ko">
       <body className={`${pressStart2p.variable} ${fredoka.variable}`}>{children}</body>
     </html>
   );
