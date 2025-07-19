a92991f3455dfe61a71a454c4fd88a68678a25da
dasom <ds92ko@gmail.com>
2025-07-20 08:38:51 +0900

    feat: header 컴포넌트 수정
    - style 추가
    - pokeball 컴포넌트 추가
    - global bar 컴포넌트 추출

--- src/components/header/index.css.ts
+++ src/components/header/index.css.ts
@@ -3,7 +3,14 @@ import { style } from '@vanilla-extract/css';
 import { fontFamily, vars } from '@/styles/vars.css';
 
 export const header = style({
-  borderBottom: `1px solid ${vars.colors.border}`
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: vars.layout.header.height,
+  background: vars.colors.white,
+  borderBottom: `1px solid ${vars.colors.border}`,
+  zIndex: 400
 });
 
 export const content = style({
@@ -15,26 +22,21 @@ export const content = style({
 });
 
 export const logo = style({
+  flexShrink: 0
+});
+
+export const logoLink = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: vars.spacing.xs
+});
+
+export const logoText = style({
   fontFamily: `${fontFamily.logo}, sans-serif`,
   fontWeight: vars.fonts.weight.medium,
   fontSize: vars.fonts.size.xxl,
   color: vars.colors.secondary,
   WebkitTextStroke: `2px ${vars.colors.accent}`,
   paddingBottom: vars.spacing.xs
-});
-
-export const globalBar = style({
-  display: 'flex',
-  alignItems: 'center',
-  gap: vars.spacing.sm
-});
-
-export const searchBar = style({
-  width: 135
-});
-
-export const gnb = style({
-  display: 'flex',
-  alignItems: 'center',
-  gap: vars.spacing.md
 });
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,9 @@
 import Link from 'next/link';
-import { BiLogoGithub, BiSearch } from 'react-icons/bi';
 
 import Container from '@/components/container';
-import { content, globalBar, gnb, header, logo, searchBar } from '@/components/header/index.css';
-import Input from '@/components/input';
-import { button, link } from '@/styles/global.css';
-import { iconSize } from '@/styles/vars.css';
+import GlobalBar from '@/components/global-bar';
+import { content, header, logo, logoLink, logoText } from '@/components/header/index.css';
+import Pokeball from '@/components/pokeball';
 
 export default function Header() {
   return (
@@ -13,55 +11,15 @@ export default function Header() {
       <Container>
         <div className={content}>
           <h1 className={logo}>
-            <Link href="/">Pokédex</Link>
+            <Link
+              className={logoLink}
+              href="/"
+            >
+              <Pokeball size={30} />
+              <span className={logoText}>Pokédex</span>
+            </Link>
           </h1>
-          <div className={globalBar}>
-            <nav>
-              <ul className={gnb}>
-                <li>
-                  <Link
-                    href="/"
-                    className={link}
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/favorites"
-                    className={link}
-                  >
-                    Favorites
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/about"
-                    className={link}
-                  >
-                    About
-                  </Link>
-                </li>
-                <li>
-                  <a
-                    href="https://github.com/ds92ko/pokedex"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={button}
-                  >
-                    <BiLogoGithub size={iconSize.md} />
-                    GitHub
-                  </a>
-                </li>
-              </ul>
-            </nav>
-            <Input
-              start={<BiSearch size={iconSize.md} />}
-              type="search"
-              placeholder="Search"
-              className={searchBar}
-            />
-          </div>
+          <GlobalBar />
         </div>
       </Container>
     </header>
