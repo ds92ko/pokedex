380c3fd5b5cf294c574d08e54f654c174b150b21
dasom <ds92ko@gmail.com>
2025-07-20 11:47:56 +0900

    feat: favicon 변경

---
+++ src/app/icon.tsx
@@ -0,0 +1,41 @@
+import { ImageResponse } from 'next/og';
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32
+};
+export const contentType = 'image/png';
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: '100%',
+          height: '100%',
+          background:
+            'linear-gradient(to bottom, #D94C4C 0%, #D94C4C 47%, #333 47%, #333 53%, #FFF 53%, #FFF 100%)',
+          border: '2px solid #333',
+          borderRadius: '50%'
+        }}
+      >
+        <div
+          style={{
+            width: '40%',
+            height: '40%',
+            background: 'radial-gradient(circle, #333 0%, #333 40%, #FFF 40%, #FFF 100%)',
+            border: '2px solid #333',
+            borderRadius: '50%'
+          }}
+        />
+      </div>
+    ),
+    {
+      ...size
+    }
+  );
+}
