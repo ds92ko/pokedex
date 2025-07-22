7be5d4f37ccaae5a6bc6153bb86e7ec96e5329f5
dasomko <ds92ko@gmail.com>
2025-07-22 18:21:59 +0900

    feat: skeleton background style css 변수로 대체

--- src/styles/skeleton.css.ts
+++ src/styles/skeleton.css.ts
@@ -1,12 +1,14 @@
 import { keyframes, style } from '@vanilla-extract/css';
 
+import { vars } from '@/styles/vars.css';
+
 const shimmer = keyframes({
   '0%': { backgroundPosition: '-200% 0' },
   '100%': { backgroundPosition: '200% 0' }
 });
 
 export const skeleton = style({
-  background: 'linear-gradient(90deg, #eee, #f5f5f5, #eee)',
+  background: vars.gradient.skeleton,
   backgroundSize: '200% 100%',
   animation: `${shimmer} 1.5s infinite`,
   borderRadius: '4px',
