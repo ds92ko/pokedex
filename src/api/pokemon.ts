75129e3a4cfda50ddc55207413f31c9083f20a08
dasomko <ds92ko@gmail.com>
2025-07-23 19:30:50 +0900

    feat: 포켓몬 리스트 및 상세 API 요청에 Next.js ISR revalidate 옵션 추가

--- src/api/pokemon.ts
+++ src/api/pokemon.ts
@@ -1,8 +1,16 @@
+import { POKE_API_REVALIDATE } from '@/constants/pokemons';
 import { FetchPokemonDetail, FetchPokemonList } from '@/type/pokemons';
 
 export const fetchPokemonList: FetchPokemonList = async ({ pageParam = 0 }) => {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/pokemons?offset=${pageParam}`);
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/pokemons?offset=${pageParam}`,
+      {
+        next: {
+          revalidate: POKE_API_REVALIDATE
+        }
+      }
+    );
 
     if (!res.ok) throw new Error(`Failed to fetch Pokémon list: ${res.status} ${res.statusText}`);
 
@@ -18,7 +26,11 @@ export const fetchPokemonDetail: FetchPokemonDetail = async ({ queryKey }) => {
   const [_, id] = queryKey;
 
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/pokemons/${id}`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/pokemons/${id}`, {
+      next: {
+        revalidate: POKE_API_REVALIDATE
+      }
+    });
 
     if (!res.ok) throw new Error(`Failed to fetch Pokémon detail: ${res.status} ${res.statusText}`);
 
