6d3ff5fa57c9b1f4810d2b260e8cf4b7c4822269
dasomko <ds92ko@gmail.com>
2025-07-23 13:36:01 +0900

    feat: tooltip에 floating-ui 적용

--- src/components/common/tooltip/index.css.ts
+++ src/components/common/tooltip/index.css.ts
@@ -1,4 +1,4 @@
-import { style, styleVariants } from '@vanilla-extract/css';
+import { style } from '@vanilla-extract/css';
 
 import { vars } from '@/styles/vars.css';
 
@@ -9,15 +9,7 @@ export const tooltip = style({
   gap: vars.spacing.xxs
 });
 
-export const tooltipControl = style({
-  position: 'relative',
-  display: 'inline-flex',
-  alignItems: 'center',
-  justifyContent: 'center'
-});
-
 export const tooltipButton = style({
-  position: 'relative',
   display: 'inline-flex',
   alignItems: 'center',
   justifyContent: 'center',
@@ -29,8 +21,7 @@ export const tooltipButton = style({
   }
 });
 
-export const tooltipPopupBase = style({
-  position: 'absolute',
+export const tooltipPopup = style({
   width: 'max-content',
   fontSize: vars.fonts.size.xxs,
   paddingBlock: vars.spacing.xxs,
@@ -39,53 +30,5 @@ export const tooltipPopupBase = style({
   border: `1px solid ${vars.colors.border}`,
   background: vars.colors.background,
   boxShadow: `0 2px 4px ${vars.alpha.shadow}`,
-  opacity: 0,
-  visibility: 'hidden',
-  transition: 'opacity 0.3s ease-out',
-  zIndex: 10,
-  selectors: {
-    [`${tooltipButton}:hover + &, ${tooltipButton}:focus + &`]: {
-      opacity: 1,
-      visibility: 'visible'
-    }
-  }
-});
-
-export const tooltipPopup = styleVariants({
-  top: [
-    tooltipPopupBase,
-    {
-      bottom: '100%',
-      left: '50%',
-      transform: 'translateX(-50%)',
-      marginBottom: vars.spacing.xs
-    }
-  ],
-  bottom: [
-    tooltipPopupBase,
-    {
-      top: '100%',
-      left: '50%',
-      transform: 'translateX(-50%)',
-      marginTop: vars.spacing.xs
-    }
-  ],
-  left: [
-    tooltipPopupBase,
-    {
-      right: '100%',
-      top: '50%',
-      transform: 'translateY(-50%)',
-      marginRight: vars.spacing.xs
-    }
-  ],
-  right: [
-    tooltipPopupBase,
-    {
-      left: '100%',
-      top: '50%',
-      transform: 'translateY(-50%)',
-      marginLeft: vars.spacing.xs
-    }
-  ]
+  zIndex: 500
 });
--- src/components/common/tooltip/index.tsx
+++ src/components/common/tooltip/index.tsx
@@ -1,35 +1,76 @@
 'use client';
 
-import { ReactNode } from 'react';
+import {
+  autoUpdate,
+  flip,
+  offset,
+  Placement,
+  shift,
+  useDismiss,
+  useFloating,
+  useFocus,
+  useHover,
+  useInteractions,
+  useRole
+} from '@floating-ui/react';
+import { ReactNode, useState } from 'react';
 import { BiInfoCircle } from 'react-icons/bi';
 
-import {
-  tooltip,
-  tooltipButton,
-  tooltipControl,
-  tooltipPopup
-} from '@/components/common/tooltip/index.css';
+import { tooltip, tooltipButton, tooltipPopup } from '@/components/common/tooltip/index.css';
 import { icons } from '@/styles/vars.css';
 
 interface TooltipProps {
   children: ReactNode;
   content: ReactNode;
-  placement?: 'top' | 'bottom' | 'left' | 'right';
+  placement?: Placement;
 }
 
 export default function Tooltip({ children, content, placement = 'right' }: TooltipProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { refs, floatingStyles, context } = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    placement,
+    middleware: [
+      offset(8),
+      flip({
+        fallbackPlacements: ['bottom', 'top', 'left']
+      }),
+      shift()
+    ],
+    whileElementsMounted: autoUpdate
+  });
+
+  const hover = useHover(context);
+  const focus = useFocus(context);
+  const dismiss = useDismiss(context);
+  const role = useRole(context, {
+    role: 'label'
+  });
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([hover, focus, dismiss, role]);
+
   return (
     <div className={tooltip}>
       {children}
-      <div className={tooltipControl}>
-        <button
-          className={tooltipButton}
-          aria-label="Tooltip"
+      <button
+        ref={refs.setReference}
+        className={tooltipButton}
+        {...getReferenceProps()}
+      >
+        <BiInfoCircle size={icons.size.lg} />
+      </button>
+      {isOpen && (
+        <div
+          ref={refs.setFloating}
+          style={floatingStyles}
+          className={tooltipPopup}
+          {...getFloatingProps()}
         >
-          <BiInfoCircle size={icons.size.lg} />
-        </button>
-        <div className={tooltipPopup[placement]}>{content}</div>
-      </div>
+          {content}
+        </div>
+      )}
     </div>
   );
 }
