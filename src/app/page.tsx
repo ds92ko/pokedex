f1a37f14e2c3320bfbd3a270300aa494ed25b4ec
dasomko <ds92ko@gmail.com>
2025-07-23 19:32:29 +0900

    feat: 목록 페이지에 suspense 및 prefetch boundary 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,33 @@
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { Suspense } from 'react';
 
 import { fetchPokemonList } from '@/api/pokemon';
+import PokemonItemsSkeleton from '@/app/_components/pokemon-item/skeleton';
 import PokemonList from '@/app/_components/pokemon-list';
+import { pokemonList } from '@/app/_components/pokemon-list/index.css';
 import Section from '@/components/layouts/section';
+import { PrefetchInfiniteBoundary } from '@/components/query-boundary/prefetch-infinite';
 import { POKEMON_LIST_QUERY_KEY } from '@/constants/pokemons';
-import { getQueryClient } from '@/lib/tanstack-query/get-query-client';
 
 export default async function ListPage() {
-  const queryClient = getQueryClient();
-
-  await queryClient.prefetchInfiniteQuery({
-    queryKey: POKEMON_LIST_QUERY_KEY,
-    queryFn: fetchPokemonList,
-    initialPageParam: 0
-  });
-
   return (
     <Section title="포켓몬 목록">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <PokemonList />
-      </HydrationBoundary>
+      <Suspense
+        fallback={
+          <ul className={pokemonList}>
+            <PokemonItemsSkeleton />
+          </ul>
+        }
+      >
+        <PrefetchInfiniteBoundary
+          options={{
+            queryKey: POKEMON_LIST_QUERY_KEY,
+            queryFn: fetchPokemonList,
+            initialPageParam: 0
+          }}
+        >
+          <PokemonList />
+        </PrefetchInfiniteBoundary>
+      </Suspense>
     </Section>
   );
 }
