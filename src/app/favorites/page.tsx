5ea4152369dde725940d49985c35fb344c362dd6
dasomko <ds92ko@gmail.com>
2025-07-23 19:27:09 +0900

    refactor: 불필요한 async 제거

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,5 +1,5 @@
 import Section from '@/components/layouts/section';
 
-export default async function FavoritesPage() {
+export default function FavoritesPage() {
   return <Section title="Favorites">즐겨찾기한 포켓몬 목록 페이지</Section>;
 }
