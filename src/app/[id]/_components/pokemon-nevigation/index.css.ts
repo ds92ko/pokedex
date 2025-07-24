319335399a87d80e5f39727f1c25df9860f7b0d4
dasomko <ds92ko@gmail.com>
2025-07-24 20:24:26 +0900

    feat: 포켓몬 상세 > 네비게이션 컴포넌트 추출

---
+++ src/app/[id]/_components/pokemon-nevigation/index.css.ts
@@ -0,0 +1,87 @@
+import { style } from '@vanilla-extract/css';
+
+import { POKEMON_NAV_IMAGE_SIZE } from '@/app/[id]/_components/pokemon-nevigation/constants';
+import { vars } from '@/styles/vars.css';
+
+export const pokemonNavList = style({
+  display: 'grid',
+  gridTemplateColumns: '1fr minmax(20%, auto) 1fr'
+});
+
+export const pokemonNavItem = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  border: `1px solid ${vars.colors.white}`,
+  padding: vars.spacing.sm,
+  background: vars.colors.accent,
+  color: vars.colors.white,
+  transition: `background 0.3s ease-out`,
+  selectors: {
+    '&:hover, &:active, &:focus, &.disabled': {
+      background: vars.alpha.accent
+    },
+    '&:not(:first-of-type)': {
+      borderLeft: 0
+    },
+    '&:first-of-type': {
+      borderRadius: `${POKEMON_NAV_IMAGE_SIZE}px 0 0 ${POKEMON_NAV_IMAGE_SIZE}px`
+    },
+    '&:nth-of-type(2)': {
+      textAlign: 'center'
+    },
+    '&:last-of-type': {
+      textAlign: 'right',
+      borderRadius: `0 ${POKEMON_NAV_IMAGE_SIZE}px ${POKEMON_NAV_IMAGE_SIZE}px 0`
+    }
+  }
+});
+
+export const pokemonNavLink = style({
+  width: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  gap: vars.spacing.sm,
+  fontWeight: vars.fonts.weight.medium,
+  selectors: {
+    [`${pokemonNavItem}:nth-of-type(2) &`]: {
+      justifyContent: 'center'
+    },
+    [`${pokemonNavItem}:last-of-type &`]: {
+      flexDirection: 'row-reverse'
+    },
+    [`${pokemonNavItem}.disabled &`]: {
+      cursor: 'not-allowed'
+    }
+  }
+});
+
+export const pokemonNavLinkImage = style({
+  background: vars.colors.white,
+  borderRadius: '50%',
+  padding: vars.spacing.xs,
+  width: POKEMON_NAV_IMAGE_SIZE,
+  height: POKEMON_NAV_IMAGE_SIZE,
+  '@media': {
+    'screen and (max-width: 480px)': {
+      padding: vars.spacing.xxs,
+      width: 50,
+      height: 50
+    }
+  }
+});
+
+export const pokemonNavLinkText = style({
+  '@media': {
+    'screen and (max-width: 480px)': {
+      display: 'none'
+    }
+  }
+});
+
+export const pokemonNavLinkSpan = style({
+  display: 'inline-block',
+  width: '100%',
+  fontSize: vars.fonts.size.sm,
+  fontWeight: vars.fonts.weight.normal
+});
---
+++ src/app/[id]/_components/pokemon-nevigation/index.tsx
@@ -0,0 +1,99 @@
+'use client';
+
+import { useSuspenseQuery } from '@tanstack/react-query';
+import Link from 'next/dist/client/link';
+import Image from 'next/image';
+import { useParams } from 'next/navigation';
+
+import { fetchPokemonNeighbor } from '@/api/pokemon';
+import { POKEMON_NAV_IMAGE_SIZE } from '@/app/[id]/_components/pokemon-nevigation/constants';
+import {
+  pokemonNavItem,
+  pokemonNavLink,
+  pokemonNavLinkImage,
+  pokemonNavLinkSpan,
+  pokemonNavLinkText,
+  pokemonNavList
+} from '@/app/[id]/_components/pokemon-nevigation/index.css';
+import Pokeball from '@/components/common/pokeball';
+import { POKEMON_NEIGHBOR_QUERY_KEY } from '@/constants/pokemons';
+
+export default function PokemonNavigation() {
+  const { id } = useParams();
+
+  const { data } = useSuspenseQuery({
+    queryKey: POKEMON_NEIGHBOR_QUERY_KEY(id),
+    queryFn: fetchPokemonNeighbor
+  });
+
+  return (
+    <nav>
+      <ul className={pokemonNavList}>
+        <li className={`${pokemonNavItem} ${data.prev ? '' : 'disabled'}`}>
+          {data.prev ? (
+            <Link
+              className={pokemonNavLink}
+              href={`/${data.prev.id}`}
+            >
+              <Image
+                className={pokemonNavLinkImage}
+                src={data.prev.image}
+                alt={data.prev.name}
+                width={POKEMON_NAV_IMAGE_SIZE}
+                height={POKEMON_NAV_IMAGE_SIZE}
+              />
+              <div className={pokemonNavLinkText}>
+                <span className={pokemonNavLinkSpan}>이전</span>
+                <strong>{data.prev.name}</strong>
+              </div>
+            </Link>
+          ) : (
+            <div className={pokemonNavLink}>
+              <Pokeball size={POKEMON_NAV_IMAGE_SIZE} />
+              <div className={pokemonNavLinkText}>
+                <span className={pokemonNavLinkSpan}>이전</span>
+                <strong>X</strong>
+              </div>
+            </div>
+          )}
+        </li>
+        <li className={pokemonNavItem}>
+          <Link
+            className={pokemonNavLink}
+            href="/"
+          >
+            목록
+          </Link>
+        </li>
+        <li className={`${pokemonNavItem} ${data.next ? '' : 'disabled'}`}>
+          {data.next ? (
+            <Link
+              className={pokemonNavLink}
+              href={`/${data.next.id}`}
+            >
+              <Image
+                className={pokemonNavLinkImage}
+                src={data.next.image}
+                alt={data.next.name}
+                width={POKEMON_NAV_IMAGE_SIZE}
+                height={POKEMON_NAV_IMAGE_SIZE}
+              />
+              <div className={pokemonNavLinkText}>
+                <span className={pokemonNavLinkSpan}>다음</span>
+                <strong>{data.next.name}</strong>
+              </div>
+            </Link>
+          ) : (
+            <div className={pokemonNavLink}>
+              <Pokeball size={POKEMON_NAV_IMAGE_SIZE} />
+              <div className={pokemonNavLinkText}>
+                <span className={pokemonNavLinkSpan}>다음</span>
+                <strong>X</strong>
+              </div>
+            </div>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+}
