57a65522e3569fa67fb65bbff3d5873d685798f6
dasomko <ds92ko@gmail.com>
2025-07-29 15:11:49 +0900

    feat: 포획한 포켓몬 페이지에 metadata 추가

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,6 +1,17 @@
+import { Metadata } from 'next';
+
 import ControlBar from '@/app/favorites/_components/control-bar';
 import FavoriteList from '@/app/favorites/_components/favorite-list';
 import Section from '@/components/layouts/section';
+
+export const metadata: Metadata = {
+  title: '포획한 포켓몬',
+  description: 'Pokédex에서 포획한 나만의 포켓몬 목록을 확인하세요.',
+  openGraph: {
+    title: '포획한 포켓몬',
+    description: 'Pokédex에서 포획한 나만의 포켓몬 목록을 확인하세요.'
+  }
+};
 
 export default function FavoritesPage() {
   return (
