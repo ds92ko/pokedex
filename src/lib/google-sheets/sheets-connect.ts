212dfd1cf9011a5802be7a9dbb87611fee583ecd
dasomko <ds92ko@gmail.com>
2025-08-18 21:51:58 +0900

    chore: 구글 스프레드시트 서비스 어카운트 base64로 관리

--- src/lib/google-sheets/sheets-connect.ts
+++ src/lib/google-sheets/sheets-connect.ts
@@ -1,10 +1,13 @@
 import { google } from 'googleapis';
 
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.GOOGLE_SERVICE_ACCOUNT ?? '', 'base64').toString('utf-8')
+);
 
 const auth = new google.auth.JWT({
-  email: process.env.GOOGLE_CLIENT_EMAIL,
-  key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  email: serviceAccount.client_email,
+  key: serviceAccount.private_key,
   scopes: SCOPES
 });
 
