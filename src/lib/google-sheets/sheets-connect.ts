b4e3c2822e7c8b085dd2c6c6706bccb4bd74bd24
dasomko <ds92ko@gmail.com>
2025-08-18 20:49:23 +0900

    feat: 구글 프라이빗 키 인코딩

--- src/lib/google-sheets/sheets-connect.ts
+++ src/lib/google-sheets/sheets-connect.ts
@@ -4,7 +4,7 @@ const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 
 const auth = new google.auth.JWT({
   email: process.env.GOOGLE_CLIENT_EMAIL,
-  key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  key: Buffer.from(process.env.GOOGLE_PRIVATE_KEY || '', 'base64').toString('utf-8'),
   scopes: SCOPES
 });
 
